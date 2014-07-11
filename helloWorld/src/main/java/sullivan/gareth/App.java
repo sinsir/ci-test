b2788d7cd6c73f0a90428d51af1bc466bcc0885e
Gareth Sullivan <gareth.sullivan@dwint.com>
2014-07-11 10:19:31 +0100

    modified branch in hope that jenkins will automerge master to feature branch

--- helloWorld/src/main/java/sullivan/gareth/App.java
+++ helloWorld/src/main/java/sullivan/gareth/App.java
@@ -12,6 +12,11 @@ public class App
 		return "Hello World, Gareth here!";
 	}
 
+	public String getHelloWorldString2()
+	{
+		return "Hello World, master branch here!";
+	}
+
     public static void main( String[] args )
     {
     	App myApp = new App();
