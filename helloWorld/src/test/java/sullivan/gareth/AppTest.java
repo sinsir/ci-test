f5fe3ebaa3f069e3b25a8db85b214df8484e09f2
Gareth Sullivan <gareth.sullivan@dwint.com>
2014-07-11 10:49:56 +0100

    Revert "Revert "modified branch in hope that jenkins will automerge master to feature branch""
    This reverts commit 65257fd621e9c299cda9e49ecdd4165c086838aa.

--- helloWorld/src/test/java/sullivan/gareth/AppTest.java
+++ helloWorld/src/test/java/sullivan/gareth/AppTest.java
@@ -19,4 +19,15 @@ public class AppTest
         assertNotNull(test);
         assertEquals("Hello World, Gareth here!",test.getHelloWorldString());
     }
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testgetHelloWorldString2()
+    {
+        App test = new App();
+        assertNotNull(test);
+        assertEquals("Hello World, master branch here!",test.getHelloWorldString2());
+    }
 }
